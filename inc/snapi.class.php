71cd16952e5575c14d6fc3a541587f879825445e
Liam Jack <cuonic@cuonic.com>
2015-08-08 14:16:30 +0200

    Moved to encryptedPasswd

--- inc/snapi.class.php
+++ inc/snapi.class.php
@@ -60,6 +60,37 @@ class Snapi
         return $return;
     }
 
+    private function getEncryptedPasswd($google_email, $google_password)
+    {
+        require 'phpseclib/Math/BigInteger.php';
+        require 'phpseclib/Crypt/RSA.php';
+
+        $googleDefaultPublicKey = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ==";
+        $binaryKey = bin2hex(base64_decode($googleDefaultPublicKey));
+
+        $half = substr($binaryKey, 8, 256);
+        $modulus  = new Math_BigInteger(hex2bin($half), 256);
+
+        $half = substr($binaryKey, 272, 6);
+        $exponent = new Math_BigInteger(hex2bin($half), 256);
+
+        $sha1  = sha1(base64_decode($googleDefaultPublicKey), true);
+        $signature = "00" . bin2hex(substr($sha1, 0, 4));
+
+        $rsa = new Crypt_RSA();
+
+        $rsa->setPublicKeyFormat(CRYPT_RSA_PUBLIC_FORMAT_RAW);
+        $rsa->loadKey(array("n" => $modulus, "e" => $exponent));
+        $rsa->setPublicKey();
+
+        $plain = "{$google_email}\x00{$google_password}";
+        $rsa->setEncryptionMode("CRYPT_RSA_ENCRYPTION_OAEP");
+        $encrypted = bin2hex($rsa->encrypt($plain));
+
+        $output = hex2bin($signature . $encrypted);
+        return str_replace(array("+", "/"), array("-", "_"), mb_convert_encoding(base64_encode($output), "US-ASCII"));
+    }
+
     private function getGoogleAuthToken()
     {
         if(!isset($this->google_device_id)) {
@@ -89,7 +120,7 @@ class Snapi
             "callerPkg" => "com.snapchat.android",
             "callerSig" => "49f6badb81d89a9e38d65de76f09355071bd67e7",
             "Email" => $this->google_email,
-            "Passwd" => $this->google_password
+            "EncryptedPasswd" => $this->getEncryptedPasswd($this->google_email, $this->google_password)
         )));
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     		'Accept:',
